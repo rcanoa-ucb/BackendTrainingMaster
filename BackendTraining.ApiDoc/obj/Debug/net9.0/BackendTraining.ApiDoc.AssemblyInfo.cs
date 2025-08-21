3ba675052f653f2173d4157daf8eb5912e843c34
Richard Canoa <rcanoa@ucb.cba.edu.bo>
2025-08-21 15:19:34 -0400

    Primer Commit

--- BackendTraining.ApiDoc/obj/Debug/net9.0/BackendTraining.ApiDoc.AssemblyInfo.cs
+++ BackendTraining.ApiDoc/obj/Debug/net9.0/BackendTraining.ApiDoc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendTraining.ApiDoc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f2b2d5cf3dd1b4fa9af17cc82ed29a0028f62bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendTraining.ApiDoc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendTraining.ApiDoc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
